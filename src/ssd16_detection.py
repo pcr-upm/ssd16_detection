2c6bc047c3859b347e67ceed270868ee621d30f4
Roberto Valle <roberto.valle@upm.es>
2023-07-24 13:34:50 +0200

    Added Person detector

--- src/ssd16_detection.py
+++ src/ssd16_detection.py
@@ -5,7 +5,7 @@ __email__ = 'roberto.valle@upm.es'
 
 import os
 import cv2
-from images_framework.src.annotations import FaceObject, GenericCategory
+from images_framework.src.annotations import PersonObject, FaceObject, GenericCategory
 from images_framework.src.detection import Detection
 from images_framework.src.categories import Category as Oi
 
@@ -19,6 +19,12 @@ class SSD16Detection(Detection):
         self.path = path
         self.model = None
         self.gpu = None
+        self.width = 300
+        self.height = 300
+        self.filename = None
+        self.category = None
+        self.mean = None
+        self.std = None
 
     def parse_options(self, params):
         super().parse_options(params)
@@ -29,6 +35,22 @@ class SSD16Detection(Detection):
         args, unknown = parser.parse_known_args(params)
         print(parser.format_usage())
         self.gpu = args.gpu
+        if self.database in ['coco', 'pascal_voc']:
+            # The model was created with SSD framework using MobileNet like architecture as a backbone.
+            # The model was first trained on the COCO dataset and was then fine-tuned on PASCAL VOC.
+            self.filename = 'mobilenet'
+            self.category = Oi.PERSON
+            self.mean = (127.5, 127.5, 127.5)
+            self.std = 0.007843
+        elif self.database in ['300w_public', '300w_private', 'cofw', 'aflw', 'wflw', 'dad', '300wlp']:
+            # The model was created with SSD framework using ResNet-10 like architecture as a backbone.
+            # The model was trained in Caffe framework on some huge and available online dataset.
+            self.filename = 'res10_300x300_ssd'
+            self.category = Oi.FACE
+            self.mean = (104.0, 177.0, 123.0)
+            self.std = 1.0
+        else:
+            raise ValueError('Database is not implemented')
 
     def train(self, anns_train, anns_valid):
         import caffe
@@ -39,10 +61,10 @@ class SSD16Detection(Detection):
             caffe.set_mode_gpu()
         # Create Caffe neural network
         print('Running solver ...')
-        solver_file = self.path + 'data/solver.prototxt'
+        solver_file = self.path + 'data/' + self.category.label.name + '/' + 'solver.prototxt'
         solver = caffe.get_solver(solver_file)
         # Train from scratch vs Finetune from a previous net
-        model_file = self.path + 'data/res10_300x300_ssd.caffemodel'
+        model_file = self.path + 'data/' + self.category.label.name + '/' + self.filename + '.caffemodel'
         if os.path.exists(model_file):
             solver.net.copy_from(model_file)
         solver.solve()
@@ -51,8 +73,8 @@ class SSD16Detection(Detection):
         from images_framework.src.constants import Modes
         # Set up a neural network to train
         print('Load model')
-        proto_file = self.path + 'data/deploy.prototxt'
-        model_file = self.path + 'data/res10_300x300_ssd_iter_140000.caffemodel'
+        proto_file = self.path + 'data/' + self.category.name + '/' + 'deploy.prototxt'
+        model_file = self.path + 'data/' + self.category.name + '/' + self.filename + '.caffemodel'
         if mode is Modes.TEST:
             self.model = cv2.dnn.readNetFromCaffe(proto_file, model_file)
 
@@ -60,14 +82,14 @@ class SSD16Detection(Detection):
         for img_pred in pred.images:
             # Load image
             image = cv2.imread(img_pred.filename)
-            input_blob = cv2.dnn.blobFromImage(image, scalefactor=1.0, size=(300, 300), mean=(104.0, 177.0, 123.0), swapRB=False, crop=False)
+            input_blob = cv2.dnn.blobFromImage(image, scalefactor=self.std, size=(self.width, self.height), mean=self.mean, swapRB=False, crop=False)
             self.model.setInput(input_blob, 'data')
             output = self.model.forward()
             # Save prediction
             for detection in output[0][0]:
                 if detection[2] < 0.5:
                     continue
-                obj = FaceObject()
+                obj = PersonObject() if self.category == Oi.PERSON else FaceObject()
                 obj.bb = tuple(detection[3:7] * [image.shape[1], image.shape[0], image.shape[1], image.shape[0]])
-                obj.add_category(GenericCategory(label=Oi.FACE, score=detection[2]))
+                obj.add_category(GenericCategory(label=self.category, score=detection[2]))
                 img_pred.add_object(obj)
