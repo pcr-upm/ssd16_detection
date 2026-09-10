5c7218f2fdb86cc5234f31dc2caf62ed3b2815c6
Roberto Valle <roberto.valle@upm.es>
2026-09-10 12:21:24 +0200

    Use SSH forwarding for secure private repository cloning

--- test_ssd16_detection_example.sh
+++ test_ssd16_detection_example.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 echo 'Using Docker to start the container and run tests ...'
-sudo docker build --force-rm --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -t ssd16_detection_image .
-sudo docker volume create --name ssd16_detection_volume
-sudo docker run --name ssd16_detection_container -v ssd16_detection_volume:/home/username --rm --gpus all -it -d ssd16_detection_image bash
+sudo docker build --force-rm --ssh default=$HOME/.ssh/id_rsa -t ssd16_detection_image .
+sudo docker run --name ssd16_detection_container --rm --gpus all -it -d ssd16_detection_image bash
 sudo docker exec -w /home/username/ssd16_detection ssd16_detection_container python test/ssd16_detection_test.py --input-data test/example.tif --database aflw --gpu 0 --save-image
-sudo docker stop ssd16_detection_container
 echo 'Transferring data from docker container to your local machine ...'
 mkdir -p output
-sudo chown -R "${USER}":"${USER}" /var/lib/docker/
-rsync --delete -azvv /var/lib/docker/volumes/ssd16_detection_volume/_data/conda/envs/ssd16/lib/python3.8/site-packages/images_framework/output/images/ output
-sudo docker system prune --all --force --volumes
-sudo docker volume rm $(sudo docker volume ls -qf dangling=true)
+sudo docker cp ssd16_detection_container:/home/username/conda/envs/ssd16/lib/python3.8/site-packages/images_framework/output/images/. output/
+sudo chown -R "${USER}":"${USER}" output
+sudo docker rm -f ssd16_detection_container
+sudo docker image rm ssd16_detection_image
+sudo docker builder prune -a -f
